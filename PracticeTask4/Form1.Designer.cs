5f231158d83176a9c4f6219e66031b15c3363bf5
skjfdnv <skjfdnv@yandex.ru>
2020-05-25 23:33:33 +0500

    Upd

--- PracticeTask4/Form1.Designer.cs
+++ PracticeTask4/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SumOutput = new System.Windows.Forms.Label();
+            this.eInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(10, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите точность e:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(10, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Сумма равна:";
+            this.label2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::PracticeTask4.Properties.Resources.Sum;
+            this.pictureBox1.Location = new System.Drawing.Point(304, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 135);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(64, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 43);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Подсчет суммы:";
+            // 
+            // SumOutput
+            // 
+            this.SumOutput.AutoSize = true;
+            this.SumOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(245)))), ((int)(((byte)(248)))));
+            this.SumOutput.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SumOutput.Location = new System.Drawing.Point(169, 299);
+            this.SumOutput.Name = "SumOutput";
+            this.SumOutput.Size = new System.Drawing.Size(0, 33);
+            this.SumOutput.TabIndex = 4;
+            this.SumOutput.Visible = false;
+            // 
+            // eInput
+            // 
+            this.eInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(245)))), ((int)(((byte)(248)))));
+            this.eInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.eInput.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.eInput.Location = new System.Drawing.Point(256, 192);
+            this.eInput.Name = "eInput";
+            this.eInput.Size = new System.Drawing.Size(200, 41);
+            this.eInput.TabIndex = 5;
+            this.eInput.TextChanged += new System.EventHandler(this.eInput_TextChanged);
+            this.eInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.eInput_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.BackgroundImage = global::PracticeTask4.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(468, 426);
+            this.Controls.Add(this.eInput);
+            this.Controls.Add(this.SumOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1181, 465);
+            this.Name = "Form1";
+            this.Text = "Сумма ряда";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label SumOutput;
+        private System.Windows.Forms.TextBox eInput;
     }
 }
 
--- PracticeTask4/Form1.cs
+++ PracticeTask4/Form1.cs
@@ -12,9 +12,70 @@ namespace PracticeTask4
 {
     public partial class Form1 : Form
     {
+        double e, sum = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void Sum_Calculation()
+        {
+            double current_term = 0;
+            sum = 0;
+            int i = 1;
+            do
+            {
+                current_term = 1 / (Math.Pow(i, 2));
+                sum += current_term;
+                i++;
+            } while (Math.Abs(current_term) >= e);
+            SumOutput.Text = sum.ToString();
+            int rounding = 10;
+            if (SumOutput.Location.X + SumOutput.Size.Width >= this.Width)
+            {
+                sum = Math.Round(sum, rounding);
+                SumOutput.Text = sum.ToString();
+            }
+            SumOutput.Focus();
+        }
+
+        private void eInput_TextChanged(object sender, EventArgs e)
+        {
+            if (SumOutput.Text != string.Empty)
+            {
+                label2.Visible = false;
+                SumOutput.Visible = false;
+                SumOutput.Text = string.Empty;
+            }
+        }
+
+        private void eInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (SumOutput.Text != string.Empty)
+                {
+                    label2.Visible = false;
+                    SumOutput.Visible = false;
+                    SumOutput.Text = string.Empty;
+                }
+                eInput.Text = eInput.Text.Replace('.', ',');
+                if (double.TryParse(eInput.Text, out this.e))
+                {
+                    if (this.e > 0)
+                    {
+                        label2.Visible = true;
+                        SumOutput.Visible = true;
+                        Sum_Calculation();
+                    }
+                    else MessageBox.Show("Введите положительное число!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else MessageBox.Show("Введите положительное число!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
